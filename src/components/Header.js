fc9aa6659d6c8e10bcb04cc36435f9d73c4b942e
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-06-24 19:25:55 -0300

    Header estilizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import '../components/header.css';
+import carteira from '../images/carteira.png';
+import perfil from '../images/perfil.png';
+
 class Header extends React.Component {
   calTotalExpenses = () => {
     const { expenses } = this.props;
 
     const total = expenses.reduce((acc, expense) => {
-      const convertedValue = expense.value * expense.exchangeRates[expense.currency].ask;
+      const convertedValue =
+        expense.value * expense.exchangeRates[expense.currency].ask;
 
       const sum = acc + convertedValue;
 
@@ -22,10 +27,32 @@ class Header extends React.Component {
 
     return (
       <div>
-        <header>
-          <p data-testid="email-field">{ email }</p>
-          <p data-testid="total-field">{ this.calTotalExpenses() }</p>
-          <p data-testid="header-currency-field">BRL</p>
+        <header className='header'>
+          <div className='title-container'>
+            <img
+              className='wallet'
+              src={carteira}
+              alt='Desenho de uma carteira'
+            />
+            <h2 className='header-title'>
+              TR<span>Y</span>BE WALLET
+            </h2>
+          </div>
+          <div className='wallet-container'>
+            <div className='wallet-email'>
+              <img className='user-image' src={perfil} alt='Imagem de perfil' />
+              <p className='user-email' data-testid='email-field'>
+                {email}
+              </p>
+            </div>
+            <div className='wallet-funds'>
+              R$:
+              <span className='user-funds' data-testid='total-field'>
+                {this.calTotalExpenses()}
+              </span>
+              <span data-testid='header-currency-field'>BRL</span>
+            </div>
+          </div>
         </header>
       </div>
     );
